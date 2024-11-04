e54742739f534187dacdd21f98a63856ef0b6eff
Humam EL-Masri <humam-m@JoyBox.Corp>
2024-11-04 14:29:23 +0300

    finishing Level 3 part 10

--- app/Http/Filters/V1/TicketFilter.php
+++ app/Http/Filters/V1/TicketFilter.php
@@ -3,13 +3,24 @@
 namespace App\Http\Filters\V1;
 
 class TicketFilter extends QueryFilter {
-    public function gde($value){
+    public function include($value){
         return $this->builder->with($value);
     }
-
     public function status($value)
     {
-        return $this->builder->where('status',$value);
+        return $this->builder->whereIn('status',explode(',',$value));
+    }
+
+    public function title($value){
+
+        return $this->builder->where('title','LIKE','%'.  $value . '%');
+    }
+    public function createdAt($value){
+        $dates = explode(',', $value);
+        if(count($dates) > 1) {
+            return $this->builder->whereBetween('created_at', $value);
+        }
+        return $this->builder->whereDate('created_at',$value);
     }
 
 }
